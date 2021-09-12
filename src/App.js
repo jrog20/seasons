24119f8c838ee3ea60f561b12b82d94057e1b0ca
jrog20 <jaimerogstad@gmail.com>
2021-09-12 12:17:03 -0400

    Add stylesheet and set up geolocation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SeasonDisplay from './components/SeasonDisplay.js';
 
 function App() {
+  // function to get current user location
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback (first argument)
+    (position) => console.log(position),
+    // failure callback (second argument)
+    (err) => console.log(err)
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hi there!
+      <SeasonDisplay />
     </div>
   );
 }
